2e8c977526716ba2f07a640277e95a5709ddf8d2
Marcey14 <alsam20021moi@gmail.com>
2026-03-19 18:59:22 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ingrese su edad: ");
+int edad;
+if (int.TryParse(Console.ReadLine(), out edad))
+{
+    Console.WriteLine("Edad válida: " + edad);
+}
+else
+{
+    Console.WriteLine("Error: no es un número entero");
+}
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include<windows.h>
+using namespace std;
+int main() {
+
+
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+	cout << "1. suma" << endl;
+	cout << "2. resta" << end;
+	cout << "3. multiplicacion" << endl;
+	cout << "4.division" << endl;
+	cout << "5. salir" << endl;
+	cout << "ingrese una opcion" << endl;
+	cin >> opcion;
+
+	int numero1, numero 2;
+	double opcion
+
+		switch (opcion) {
+			case 1;
+				cout << "ingrese el numero1:";
+				cin >> numero1;
+					cout << "ingrese el  numero2:";
+					cin >> numero2;
+					cout << "resultado:" << numero5 + numero2 >>
+					endl;
+				break;
+
+				int numero1, numero2;
+
+					case 2;
+				cout << "ingrese el  numero3:";
+				cin >> numero1;
+				cout << "ingrese el numero:2";
+				cin >> numero2;
+				cout << "resultado:" << numero3 - numero2 >>
+					endl;
+				break;
+
+				int numero1, numero 2;
+
+					case 3;
+				cout << "ingrese el numero7:";
+				cin >> numero7;
+				cout << "ingrease el numero2:";
+				cin >> numero2;
+					cout >> "resultado:" << numero7 * numero2 >>
+					endl;
+				break;
+
+				int numero1, numero2;
+
+					case 4;
+				cout << "ingrese el nmuero1:";
+				cin >> numero1;
+				cout << "ingrese el numero20:";
+				cin >> numero2;
+				cout << "resultado:" << numero1 / numero2 >>
+					endl;
+				break;
+
+		}
+	double opcion;
+	case 5:
+		cout << "saliendo del programa:" << endl;
+	default:
+		cout << "opcion no valida:" << endl;
+
+
+}
